ae85be3ee91fa265c554faa7743c2f60a6fdf41a
刘亚军 <liuyajun1999@icloud.com>
2019-08-14 16:23:14 +0800

    初始化工程

---
+++ Example/Pods/Target Support Files/Pods-YJUtils_Tests/Pods-YJUtils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YJUtils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YJUtils_Tests
+@end
--- Example/YJUtils/YJViewController.m
+++ Example/YJUtils/YJViewController.m
@@ -7,23 +7,67 @@
 //
 
 #import "YJViewController.h"
+#import <YJUtils/YJAudioPlayer.h>
 
-@interface YJViewController ()
-
+@interface YJViewController ()<YJAudioPlayerDelegate>
+@property (nonatomic,strong) YJAudioPlayer *audioPlayer;
+@property (weak, nonatomic) IBOutlet UILabel *currentTimeLab;
+@property (weak, nonatomic) IBOutlet UIProgressView *playProgressView;
+@property (weak, nonatomic) IBOutlet UILabel *totalTimeLab;
+@property (weak, nonatomic) IBOutlet UIProgressView *bufProgressView;
 @end
 
 @implementation YJViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSString *urlStr = @"http:/192.168.129.129:10145/CoursewareResFile/bktea5/eabed9c81eed492f86877b518dbd66cf/res/0/1/f8a7660cbc854e199cde7100eb94527e/data.mp3";
+    self.audioPlayer = [[YJAudioPlayer alloc] init];
+    self.audioPlayer.delegate = self;
+    self.audioPlayer.audioUrl = urlStr;
+}
+- (IBAction)play:(id)sender {
+    [self.audioPlayer play];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playAmbient:(id)sender {
+    [self.audioPlayer playAmbient];
 }
 
+- (IBAction)pause:(id)sender {
+    [self.audioPlayer pause];
+}
+- (IBAction)stop:(id)sender {
+    [self.audioPlayer stop];
+}
+- (IBAction)sliderVauleChange:(UISlider *)sender {
+    NSTimeInterval time = sender.value * self.audioPlayer.totalDuration;
+    [self.audioPlayer seekToSecondTime:time];
+}
+
+- (void)yj_audioPlayerReadyToPlay{
+    NSLog(@"准备好了");
+    NSInteger minute = (NSInteger)self.audioPlayer.totalDuration % (60*60) / 60;
+    NSInteger second = (NSInteger)self.audioPlayer.totalDuration % (60*60) % 60;
+    self.totalTimeLab.text = [NSString stringWithFormat:@"%02li:%02li",minute,second];
+}
+
+- (void)yj_audioPlayerCurrentPlaySeconds:(NSTimeInterval)seconds progress:(CGFloat)progress{
+    NSInteger minute = (NSInteger)seconds % (60*60) / 60;
+    NSInteger second = (NSInteger)seconds % (60*60) % 60;
+    self.currentTimeLab.text = [NSString stringWithFormat:@"%02li:%02li",minute,second];
+    
+    
+    self.playProgressView.progress = progress;
+}
+- (void)yj_audioPlayerCurrentBufferSeconds:(NSTimeInterval)seconds progress:(CGFloat)progress{
+    self.bufProgressView.progress = progress;
+}
+- (void)yj_audioPlayerBeginInterruption{
+    [self.audioPlayer pause];
+    NSLog(@"开始中断");
+}
+- (void)yj_audioPlayerEndInterruption{
+    NSLog(@"结束中断");
+}
 @end
